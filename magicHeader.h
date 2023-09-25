96b689ac050535caa322a45ae70864df0331c8bc
Nicoleitor57 <115563015+Nicoleitor57@users.noreply.github.com>
2023-09-25 17:53:29 -0300

    Update magicHeader.h

--- magicHeader.h
+++ magicHeader.h
@@ -5,6 +5,10 @@
 #define COLS 5
 #define SIZE 3
 
+#define NUM_ROWS 4
+#define NUM_COLS 5
+#define MAX_FILENAME_LENGTH 20
+
 
 struct jugador
 {
@@ -21,12 +25,20 @@ struct tablero
     char data[ROWS][COLS][SIZE];
 };
 
+int randomIndex(int maxIndex);
 
 void moveRight(struct jugador *jugador, struct tablero *tablero);
 void moveLeft(struct jugador *jugador, struct tablero *tablero);
 void moveUp(struct jugador *jugador, struct tablero *tablero);
 void moveDown(struct jugador *jugador, struct tablero *tablero);
 
+void removeFromAllArrays(char arr[][NUM_COLS][MAX_FILENAME_LENGTH], int numRows, int numCols, const char *itemToRemove, int removed[][NUM_COLS]);
+const char *selectRandomItem(char arr[][NUM_COLS][MAX_FILENAME_LENGTH], int numRows, int numCols, int removed[][NUM_COLS], int rowIndex);
+const char *selectRandomItemFromRow(char fileNames[][NUM_COLS][MAX_FILENAME_LENGTH], int rowIndex);
+void Bsearch(struct jugador *jugador, struct tablero *tablero,char *(*mapData)[5], int turno);
+
+
+void populateTablero(struct tablero *tablero, const char *filename, int turno);
 void printTablero(struct tablero *tablero);
 
 
